995e31a162bdafd3aa2b36715d05981e3fa8838a
meetmahajan <monoarchos@gmail.com>
2019-09-19 14:15:06 +0530

    Customer details and CutomerList created

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,9 +1,13 @@
+import { CustomersDetailsComponent } from './customers-details/customers-details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CustomersComponent } from './customers.component';
 
-const routes: Routes = [{ path: '', component: CustomersComponent }];
+const routes: Routes = [
+  { path: '', component: CustomersComponent },
+  { path: ':id', component: CustomersDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
+import { CustomersDetailsComponent } from './customers-details/customers-details.component';
 
 
 @NgModule({
-  declarations: [CustomersComponent],
+  declarations: [CustomersComponent, CustomersDetailsComponent],
   imports: [
     CommonModule,
     CustomersRoutingModule
