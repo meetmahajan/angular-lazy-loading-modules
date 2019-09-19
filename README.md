375a57502857c2114a85172afbfefed6a6fdc89e
meetmahajan <monoarchos@gmail.com>
2019-09-19 11:52:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-lazy-loading-modules-tuen
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-lazy-loading-modules-tuen)
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
