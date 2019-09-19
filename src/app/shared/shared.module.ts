3bb1d3b29e3c75c3f010dee9e834c3ba92796c9a
meetmahajan <monoarchos@gmail.com>
2019-09-19 13:56:28 +0530

    Highlight directives and shared folder configuration.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HighlightDirective } from './directive/highlight.directive';
+
+
+
+@NgModule({
+  declarations: [HighlightDirective],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HighlightDirective
+  ]
+})
+export class SharedModule { }
